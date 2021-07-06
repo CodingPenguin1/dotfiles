7ab36d1a20e4ecf5bd358070d78d5fa3cb2cd654
Ryan Slater <ryan.j.slater.2@gmail.com>
2021-07-06 15:56:41 -0400

    Delete .vscode/extensions/apommel.matlab-interactive-terminal-0.3.4 directory

--- .vscode/extensions/apommel.matlab-interactive-terminal-0.3.4/interfaces/standard/ml_script.py
+++
@@ -1,9 +0,0 @@
-# Developed by Aurelien Pommel and other contributors
-
-from matlab_interface import MatlabInterface
-import sys
-
-matlab = MatlabInterface()
-# Run the initial script given in arg
-matlab.run_script(sys.argv[1])
-matlab.interactive_loop()
